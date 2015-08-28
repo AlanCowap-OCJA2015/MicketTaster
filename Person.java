b8224734344de51125c20215d5a017e24f51efc4
ThomasMcSherry <thomasmcsherry92@gmail.com>
2015-08-28 10:19:04 +0100

    Update Person.java
    Allowed to add tickets

--- Person.java
+++ Person.java
@@ -10,18 +10,27 @@ public class Person {
 	Person(StringBuilder name){
 		this(name, new StringBuilder("0000_0000_0000_0000"));
 	}
+	
 	Person(StringBuilder name, StringBuilder creditCardNum){
 		this(name, creditCardNum, new StringBuilder("joe@bloggs.com"));
 	}
+	
 	Person(StringBuilder name, StringBuilder creditCardNum, StringBuilder email){
 		this(name, creditCardNum, new StringBuilder("joe@bloggs.com"), new ArrayList<Ticket>() );
 	}
+	
 	Person(StringBuilder name, StringBuilder creditCardNum, 
 			StringBuilder email, ArrayList<Ticket> tickets){
 		this.name = name;
 		this.creditCardNum = creditCardNum;
 		this.email = email;
 		this.tickets = tickets;
+	}
+	
+	
+	public void addTicket(int price, Event e){
+		
+		tickets.add(new Ticket(price,e.getEventName()));
 	}
 
 }
